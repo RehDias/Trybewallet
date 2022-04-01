5d3c5a379b07bc940b94a5f6dcd5db642a2f5ed8
Renata Dias <renata@pop-os.localdomain>
2022-04-01 17:10:56 -0300

    Finalizado requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,4 @@
 // Coloque aqui suas actions
+export const GET_EMAIL = 'EMAIL';
+
+export const actionEmail = (payload) => ({ type: GET_EMAIL, payload });
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,89 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { actionEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      senha: '',
+      isBtnDisabled: true,
+    };
+  }
+
+  handleInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, this.validateInput);
+  }
+
+  validateInput = () => {
+    const { email, senha } = this.state;
+    const minCaractereSenha = 5;
+    const regexEmail = /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/g; // https://regexr.com/3e48o
+    const validateEmail = regexEmail.test(email);
+    const validatePassword = senha.length > minCaractereSenha;
+    const validation = validateEmail && validatePassword;
+    this.setState({ isBtnDisabled: !validation });
+  }
+
+  handleBtn = (event) => {
+    event.preventDefault();
+    const { email } = this.state;
+    const { history, emailDispatch } = this.props;
+    emailDispatch(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, senha, isBtnDisabled } = this.state;
+    return (
+      <form>
+        <label htmlFor="input-login">
+          Login
+          <input
+            type="text"
+            data-testid="email-input"
+            name="email"
+            id="input-login"
+            value={ email }
+            placeholder="Digite seu login"
+            onChange={ this.handleInput }
+          />
+        </label>
+        <label htmlFor="input-senha">
+          Password
+          <input
+            type="password"
+            data-testid="password-input"
+            name="senha"
+            id="input-senha"
+            value={ senha }
+            placeholder="Digite sua senha"
+            onChange={ this.handleInput }
+          />
+        </label>
+        <button
+          type="button"
+          onClick={ this.handleBtn }
+          disabled={ isBtnDisabled }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  emailDispatch: (payload) => dispatch(actionEmail(payload)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
+
+Login.propTypes = {
+  history: PropTypes.shape(Object).isRequired,
+  emailDispatch: PropTypes.func.isRequired,
+};
